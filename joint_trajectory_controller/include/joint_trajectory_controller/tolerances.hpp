8b5857f1a5dd0495b9df72de744ebddda06ca634
amrapali-rightbot <amrapali.kh@rightbot.in>
2023-05-08 13:56:52 +0530

    removed vel tolerance logging and added pos tolerance logging if > 0.05

--- joint_trajectory_controller/include/joint_trajectory_controller/tolerances.hpp
+++ joint_trajectory_controller/include/joint_trajectory_controller/tolerances.hpp
@@ -152,18 +152,24 @@ inline bool check_state_tolerance_per_joint(
     const auto logger = rclcpp::get_logger("tolerances");
     RCLCPP_ERROR(logger, "Path state tolerances failed:");
 
+    if (state_tolerance.position > 0.0 && abs(error_position) > 0.05)
+    {
+      RCLCPP_ERROR(
+        logger, "Position Error: %f, Position Logging Tolerance: 0.05", error_position);
+    }
+
     if (state_tolerance.position > 0.0 && abs(error_position) > state_tolerance.position)
     {
       RCLCPP_ERROR(
         logger, "Position Error: %f, Position Tolerance: %f", error_position,
         state_tolerance.position);
     }
-    if (state_tolerance.velocity > 0.0 && abs(error_velocity) > state_tolerance.velocity)
-    {
-      RCLCPP_ERROR(
-        logger, "Velocity Error: %f, Velocity Tolerance: %f", error_velocity,
-        state_tolerance.velocity);
-    }
+    // if (state_tolerance.velocity > 0.0 && abs(error_velocity) > state_tolerance.velocity)
+    // {
+    //   RCLCPP_ERROR(
+    //     logger, "Velocity Error: %f, Velocity Tolerance: %f", error_velocity,
+    //     state_tolerance.velocity);
+    // }
     if (
       state_tolerance.acceleration > 0.0 && abs(error_acceleration) > state_tolerance.acceleration)
     {
